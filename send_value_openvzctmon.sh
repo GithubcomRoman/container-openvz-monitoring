ee3db6793658fc8a8b540e651b6d720d5d075d50
Roman <35697476+GithubcomRoman@users.noreply.github.com>
2023-03-06 21:09:57 +0200

    Update send_value_openvzctmon.sh

--- send_value_openvzctmon.sh
+++ send_value_openvzctmon.sh
@@ -5,4 +5,4 @@ DATA="/etc/zabbix/scripts/ct_param_value.dat"
 param=$1
 value=$2
 
-cat /etc/zabbix/scripts/ct_param_value.dat | grep "$param " | awk '{print $(var)}' var="${value}"
+cat /etc/zabbix/scripts/ct_param_value.dat | grep "^$param " | awk '{print $(var)}' var="${value}"
